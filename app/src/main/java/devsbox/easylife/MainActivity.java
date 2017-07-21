bb2d93b12c6d5518d3b96c7563acd16a17078abe
jihanislam007 <jihanislam007@gmail.com>
2017-07-22 05:04:19 +0600

    Initial commit

---
+++ app/src/main/java/devsbox/easylife/MainActivity.java
@@ -0,0 +1,77 @@
+package devsbox.easylife;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ImageView MyAppoinment;
+    ImageView mail, sms;
+    Dialog mDialog;
+
+    public int psivalue=0;
+    SharedPreferences shared;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        MyAppoinment = (ImageView) findViewById(R.id.MyAppoinmentTextView);
+        MyAppoinment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pop();
+                mDialog.show();
+            }
+        });
+    }
+
+    public void pop() {
+
+        mDialog = new Dialog(MainActivity.this);
+        mDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        mDialog.setContentView(R.layout.popuap_new);
+
+        mail = (ImageView) mDialog.findViewById(R.id.imageViewCall);
+        sms = (ImageView) mDialog.findViewById(R.id.imageViewSms);
+
+
+        shared = getSharedPreferences("A", Context.MODE_PRIVATE); // get the set of Preferences labeled "A"
+        final SharedPreferences.Editor editor = shared.edit();
+
+
+        mail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(MainActivity.this , CallActivity.class);
+                startActivity(in);
+            }
+        });
+
+        sms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ////////////SharedPreferences///////////////
+                psivalue = psivalue+1;
+                editor.putInt("psi", psivalue);
+                editor.commit(); // commit it when you are done.
+
+                Intent in = new Intent(MainActivity.this , SmsActivity.class);
+                startActivity(in);
+            }
+        });
+
+    }
+
+}
